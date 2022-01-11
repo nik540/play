fa868699ba2fa1bdc1158bc946b714ad512b4609
nik540 <to.aleksandr.nik@gmail.com>
2022-01-12 00:00:35 +0200

    1

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let random = (min, max) => {
-  return (Math.random() * (max - min) + min);
+  let rand = (Math.random() * (max + 1 - min) + min);
+  return Math.floor(rand)
 }
 alert(random(1, 5));
 alert(random(1, 5));
