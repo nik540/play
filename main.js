7e79c01b7a848a7d40740f5643e303df329a93ac
nik540 <to.aleksandr.nik@gmail.com>
2022-01-19 18:41:18 +0200

    1

--- main.js
+++ main.js
@@ -2,26 +2,29 @@ let mas = [];
 let sum1 = 0;
 let sum = 0;
 let index = 0;
-let getSubSum = () => {
-  for (i = 0; i < 3; i++) {
+
+function getSubSum() {
+  for (i = 0; i < 5; i++) {
     mas[i] = +prompt();
   };
 };
-let retSubSum = function () {
+function retSubSum() {
   for (i = 0; i < mas.length; i++) {
-    if (mas[i] < 0)
-      continue;
-    for (; i < mas.length; i++)
-      sum += mas[i];
-    // for (; index < mas.length; index - 1)
-    sum1 += mas[index]
-  };
-  index = mas.length - 1;
+    if (mas[0] < 0) {
+      mas.shift();
+    }
+  }
 }
+function petSubSum() {
+  for (i = ((mas.length) - 1); i >= 0; i--) {
+    if (mas[(mas.length) - 1] < 0) {
+      mas.pop();
+    }
+  };
 
+};
 getSubSum();
 retSubSum();
+petSubSum();
 alert(mas);
-alert(sum);
-alert(index);
-alert(sum1);
+// alert(sum);
