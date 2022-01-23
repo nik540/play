5991597e479c956b42407063727ad62ecbf9d9f2
nik540 <to.aleksandr.nik@gmail.com>
2022-01-24 00:58:12 +0200

    1

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 let mas = [];
 let mas1 = [];
 let mas2 = [];
-let sum1 = 0;
+let mas3 = [];
+let mas4 = [];
 let sum = 0;
+let sum1 = 0;
+let sum2 = 0;
+let sum3 = 0;
+
 function getSubSum() {
   for (i = 0; i < 5; i++) {
     mas[i] = +prompt();
@@ -29,20 +34,31 @@ function zetSubSum() {
 }
 function metSubSum() {
   for (i = 0; i < mas.length; i++) {
-
     sum1 += mas[i];
     mas1[i] = sum1;
-    for (j = mas.length; j < 20; j++) {
-      mas1[j] = mas1[i] - mas1[i - 1];
-    }
   }
-}
+  for (i = 0; i < mas.length; i++) {
+    sum3 = sum2;
+    sum2 += mas[i];
+    mas2[i] = sum2 - sum3;
+  };
 
+  // for (i = 0; i < mas1.length; i++) {
+  // mas3.push(mas1[i] + mas2[i]);
+  // };
+  // mas4 = mas1.concat(mas2);
+  // mas4 = [].concat(mas1, mas2)
+
+};
 getSubSum();
 retSubSum();
 petSubSum();
 zetSubSum();
 metSubSum();
-alert(mas + ' mas');
-alert(sum + ' sum');
+// alert(mas + ' mas');
+// alert(sum + ' sum');
 alert(mas1 + ' mas1');
+alert(mas2 + ' mas2');
+// alert(sum1 + ' sum1');
+// alert(mas3);
+// alert(mas4);
