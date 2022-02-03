6612aa8ec47248c3b967abdee713648c82eeb652
nik540 <to.aleksandr.nik@gmail.com>
2022-02-03 20:50:41 +0200

    1

--- main.js
+++ main.js
@@ -1,33 +1,11 @@
-let mas = [];
-let mas1 = [];
-let sum = 0;
-let t = 0;
-let max = 0;
-function getSubSum() {
-  for (i = 0; i < 5; i++) {
-    mas[i] = +prompt();
+
+function filterRange(a, b) {
+  let arr1 = [];
+  for (i = 0; i < b; i++) {
+    arr1[i] = a + i;
   };
-};
-function metSubSum() {
-  for (j = 0; j < mas.length; j++) {
-    sum = 0;
-    for (i = j; i < mas.length; i++) {
-      t += 1;
-      sum += mas[i];
-      mas1[t] = sum;
-    };
-  };
-};
-function getMaxSubSum() {
-  for (i = 0; i <= t; i++) {
-    if (mas1[i] > max) {
-      max = mas1[i];
-    };
-  };
-  return max;
-};
-getSubSum();
-metSubSum();
-getMaxSubSum();
-alert(mas1 + ' mas1');
-alert(max + ' max');
+  return arr1.sort(fn)
+}
+alert(filterRange(1, 4));
+
+function fn(arr)
