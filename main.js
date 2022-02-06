01d747294a7bbccec92a47e66f88795a25de7bf5
nik540 <to.aleksandr.nik@gmail.com>
2022-02-06 20:56:41 +0200

    1

--- main.js
+++ main.js
@@ -1,11 +1,30 @@
 
-function filterRange(a, b) {
-  let arr1 = [];
-  for (i = 0; i < b; i++) {
-    arr1[i] = a + i;
-  };
-  return arr1.sort(fn)
-}
-alert(filterRange(1, 4));
 
-function fn(arr)
+const arr = [1, 2, 3, 15, 4];
+const arr1 = [2, 3, 15];
+const arr2 = [1];
+function milderRange(arr) {
+  return arr
+    .sort((a, b) => a - b)
+    .map(a => a + 1)
+    .filter(n => n % 2 == 0)
+};
+
+function filderRange(arr2) {
+  return arr2 = arr
+    .sort((a, b) => a - b)
+    .map(a => a + 1)
+    // .filter(n => n % 2 !== 0)
+    // .includes(3)
+    // .concat(arr2)
+    .forEach((a, b) => {
+      for (i = 0; i <= arr.length; i++) {
+
+      }
+    })
+};
+
+
+alert(milderRange(arr));
+alert(filderRange(arr2));
+// alert(arr2)
