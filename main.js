5e2dcf7301a8e318b6fad73d9964a720658c5950
nik540 <to.aleksandr.nik@gmail.com>
2022-02-08 12:02:34 +0200

    1

--- main.js
+++ main.js
@@ -1,30 +1,30 @@
 
-
+"use strict";
 const arr = [1, 2, 3, 15, 4];
-const arr1 = [2, 3, 15];
-const arr2 = [1];
-function milderRange(arr) {
-  return arr
-    .sort((a, b) => a - b)
-    .map(a => a + 1)
-    .filter(n => n % 2 == 0)
-};
+const arr1 = [6, 11, 7];
+const arr2 = [9, 1];
 
-function filderRange(arr2) {
+function milderRange(arr2) {
   return arr2 = arr
-    .sort((a, b) => a - b)
-    .map(a => a + 1)
-    // .filter(n => n % 2 !== 0)
-    // .includes(3)
-    // .concat(arr2)
-    .forEach((a, b) => {
-      for (i = 0; i <= arr.length; i++) {
-
-      }
-    })
+    .sort((a, b) => a - b)//изменяет исходный
+    .map(a => a + 1)//не изм исх
+    .filter(n => n % 2 == 0)//не изм исх
+    .concat(arr1)//не изм исх
+    //.includes(5)//не изм исх
+    //.reduce((total, item, index, array) => { total += item }, 0)//не изм исх
+    .reduce(function (to))
 };
+const a = arr.reduce((total, item, index, array) => {
+  total.push(item + 1)
+  return total
+}, []);//вернет новый мас
+// function filderRange(arr2) {
+// return arr2 = arr
 
 
-alert(milderRange(arr));
-alert(filderRange(arr2));
+
+alert(milderRange(arr2));
+// alert(filderRange(arr2));
 // alert(arr2)
+alert(arr);
+alert(a);
