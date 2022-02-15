71a493d9330f2518f25c28293af6b4d44196b380
nik540 <to.aleksandr.nik@gmail.com>
2022-02-15 09:31:18 +0200

    1

--- main2.js
+++ main2.js
@@ -2,29 +2,24 @@
 const arr = [1, 2, 3, 15, 3, 4];
 const arr1 = [];
 const arr2 = [];
-const arr3 = [];
-let a = 1;
-let b = 12;
-let c = 0;
-let d = 0;
+const ad = 12;
+const bt = 1;
 
-if (a < b) {
-   c = a;
-   d = b;
-   b = c;
-   a = d;
-}
+/*function miderRange(it, it1, mas1) {
+ if (it < it1) {
+    let c = it;
+    let d = it1;
+    it1 = c;
+    it = d;
+ }
+ for (let i = 0; i <= it - 1; i++) {
+    mas1[i] = it1;
+    it1 += 1;
+ }
+ return mas1;
+}*/
 
-
-function miderRange(it, it1, mas1) {
-   for (let i = 0; i <= it - 1; i++) {
-      mas1[i] = it1;
-      it1 += 1;
-   }
-   return mas1;
-}
-
-function nilderRange(mas, mas1, mas2) {
+/*  function nilderRange(mas, mas1, mas2) {
    let a = 0;
    for (let i = 0; i < mas.length; i++) {
       for (let j = 0; j < mas1.length; j++) {
@@ -36,6 +31,20 @@ function nilderRange(mas, mas1, mas2) {
       }
    }
    return mas2
-};
-alert(miderRange(a, b, arr1));
-alert(nilderRange(arr, arr1, arr2));
+}*/
+
+// function lilderRange(mas, it, it1) {
+// return mas
+// .filter(a => a <= it && a >= it1)
+// }
+
+function vilderRange(mas, it, it1) {
+   return mas
+      .forEach(a => a <= it && a >= it1)
+   // .sort((a, b) => a - b)
+}
+// alert(miderRange(ad, bt, arr1));
+// alert(nilderRange(arr, arr1, arr2));
+// alert(lilderRange(arr, ad, bt));
+alert(vilderRange(arr, ad, bt));
+alert(arr);
